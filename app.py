f2d2589f255398a0afb69410612b7f0aa4d3af35
Chinmayimullur4-art <chinmayimullur4@gmail.com>
2026-08-03 11:09:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 st.title("Checking for Eligibility to Vote")
 age=st.text_input("enter your age")
 if st.button("Submit"):
-  if age>18:
+  if age >=18 :
     st.write("Eligible for vote")
   else:
     st.write("Not Eligible")
