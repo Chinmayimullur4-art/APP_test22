7376b8ac0db800b4a3ba4fe00af72a0621845b92
Chinmayimullur4-art <chinmayimullur4@gmail.com>
2026-08-03 11:08:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import numpy as np
+
+st.title("Checking for Eligibility to Vote")
+age=st.text_input("enter your age")
+if st.button("Submit"):
+  if age>18:
+    st.write("Eligible for vote")
+  else:
+    st.write("Not Eligible")
 
